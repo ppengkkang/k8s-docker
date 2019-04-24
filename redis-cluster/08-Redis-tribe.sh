937733c1349dd31f7a12f1973ceaa4cd37e240ed
tompeng <tompeng@MacBook-Pro.local>
2019-04-24 13:23:10 +0800

    port change

--- redis-cluster/08-Redis-tribe.sh
+++ redis-cluster/08-Redis-tribe.sh
@@ -8,19 +8,19 @@ apt-get install -y vim wget python2.7 python-pip redis-tools dnsutils
 pip install redis-trib
 
 redis-trib.py create \
-  `dig +short redis-app-0.redis-service.default.svc.cluster.local`:6380 \
-  `dig +short redis-app-1.redis-service.default.svc.cluster.local`:6380 \
-  `dig +short redis-app-2.redis-service.default.svc.cluster.local`:6380
+  `dig +short redis-app-0.redis-service.default.svc.cluster.local`:6379 \
+  `dig +short redis-app-1.redis-service.default.svc.cluster.local`:6379 \
+  `dig +short redis-app-2.redis-service.default.svc.cluster.local`:6379
 
 
 redis-trib.py replicate \
-  --master-addr `dig +short redis-app-0.redis-service.default.svc.cluster.local`:6380 \
-  --slave-addr `dig +short redis-app-3.redis-service.default.svc.cluster.local`:6380
+  --master-addr `dig +short redis-app-0.redis-service.default.svc.cluster.local`:6379 \
+  --slave-addr `dig +short redis-app-3.redis-service.default.svc.cluster.local`:6379
 
 redis-trib.py replicate \
-  --master-addr `dig +short redis-app-1.redis-service.default.svc.cluster.local`:6380 \
-  --slave-addr `dig +short redis-app-4.redis-service.default.svc.cluster.local`:6380
+  --master-addr `dig +short redis-app-1.redis-service.default.svc.cluster.local`:6379 \
+  --slave-addr `dig +short redis-app-4.redis-service.default.svc.cluster.local`:6379
 
 redis-trib.py replicate \
-  --master-addr `dig +short redis-app-2.redis-service.default.svc.cluster.local`:6380 \
-  --slave-addr `dig +short redis-app-5.redis-service.default.svc.cluster.local`:6380
+  --master-addr `dig +short redis-app-2.redis-service.default.svc.cluster.local`:6379 \
+  --slave-addr `dig +short redis-app-5.redis-service.default.svc.cluster.local`:6379
